53faef087f4c8547f21e8b287f428515b793cea9
tyseschl <sebastian.schleiken@stud.hs-ruhrwest.de>
2022-06-13 21:34:45 +0200

    Datenbank Connect Tabelle fehlt noch

--- db.php
+++ db.php
@@ -4,9 +4,11 @@ $server = "db4free.net";
 $userName = "testnutzer123";
 $password = "mönchengladbach";
 
+
 $connection = new mysqli($server, $userName, $password);
 
 if($connection->connect_error){
     die("Verbindung fehlgeschlagen.".$connection->connect_error);
+    
 }
 echo "connected";
---
+++ register.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+
+
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
+<body>
+    <form action="#" method="POST">
+        Benutzername:<br>
+        <input type="text" name="username" minlength="8" maxlength="20" size="40" placeholder="Nutzername" autocomplete="off"><br>
+        Passwort:<br>
+        <input type="password" name="password" autocomplete="off" minlength="8">
+        <br>
+        Email:
+        <input type="email" name="email" autocomplete="off">
+        <br>
+        <input type="submit" name="submit" value="Registrieren">
+        <br>
+    </form>
+</body>
+</html>
